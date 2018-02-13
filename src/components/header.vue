a94d35f6fc4c63f1344f54f57ca8d3b165decdf1
waywho <walzerfan@yahoo.com>
2018-02-13 15:59:07 +0000

    logo change

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,7 +2,7 @@
   <div id="header">
     <div class="header-inner">
     <div class="logo">
-      <router-link to="/"><img src='../assets/images/logo-placeholder.png' alt="operaop logo" /></router-link>
+      <router-link to="/"><img src='../assets/images/artistcenter-logo.png' alt="artistcenter logo" /></router-link>
     </div>
 
     <ul class="header-items">
